92cc25d74fb3b601fd57567623eb229d058fe709
Muhammad Mubashir <zayansaeed111@gmail.com>
2026-09-19 21:09:05 +0500

    fix: Update navigation path in Stats component for mock interview preference

--- src/Pages/Dashboard/Stats.jsx
+++ src/Pages/Dashboard/Stats.jsx
@@ -188,7 +188,7 @@ const Stats = () => {
                         rounded="rounded-lg"
                         size="normal"
                         className="text-white font-normal cursor-pointer"
-                        onClick={() => navigate("/Mock-Interview/inprogress")}
+                        onClick={() => navigate("/Mock-Interview/preference")}
                     >
                         <img
                             src={interview}
